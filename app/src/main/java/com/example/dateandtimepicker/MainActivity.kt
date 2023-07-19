75b710fbb3ff549cea543ed0f18e44d979f2d88a
MariaLuiza-CS <m.luiza1843@gmail.com>
2023-07-19 09:17:02 -0300

    adding time picker

--- app/src/main/java/com/example/dateandtimepicker/MainActivity.kt
+++ app/src/main/java/com/example/dateandtimepicker/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.dateandtimepicker
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.DatePickerDialog
@@ -12,11 +14,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimeInput
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerDefaults
 import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.dateandtimepicker.ui.theme.DateAndTimePickerTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,11 +35,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DatePickerCompose()
+                    //DatePickerCompose()
+                    TimePickerCompose()
                 }
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimePickerCompose() {
+    val state = rememberTimePickerState()
+
+//    TimePicker(
+//        state = state,
+//        modifier = Modifier.padding(16.dp)
+//    )
+
+    TimeInput(
+        state = state,
+        modifier = Modifier.padding(16.dp)
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
